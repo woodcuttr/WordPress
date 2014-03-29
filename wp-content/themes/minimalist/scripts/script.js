8288417fe9d403961aabada7b1ee0813a2d2ffeb
woodcuttr <taille.g@gmail.com>
2014-03-29 18:46:33 +0100

    menu looking nice

---
+++ wp-content/themes/minimalist/scripts/script.js
@@ -0,0 +1,34 @@
+window.addEvent('domready', function() {
+/*var myAccordion = new Accordion($('menu'), 'h2.menuheader', 'div.menucontent', {
+opacity:true,
+display: -1,
+duration: 300,
+alwaysHide: false,
+
+		onActive: function(menuheader, menucontent){
+			menuheader.setStyle('font-weight', 'bold');
+		},
+		onBackground: function(menuheader, menucontent){
+			menuheader.setStyle('font-weight', 'bold');
+		}
+});*/
+
+var s = skrollr.init();
+
+});
+
+window.addEvent('domready', function() {
+var myAccordion = new Accordion($('maincol'), 'h2.contentheader', 'div.content', {
+opacity:true,
+display: -1,
+duration: 300,
+alwaysHide: true,
+
+		onActive: function(contentheader, content){
+			contentheader.setStyle('font-weight', 'bold');
+		},
+		onBackground: function(contentheader, content){
+			contentheader.setStyle('font-weight', 'bold');
+		}
+});
+});
---
+++ wp-content/themes/minimalist/sidebar.php
@@ -0,0 +1,15 @@
+<div id="leftcol" data-0="opacity: 1;" data-300="opacity:0; ">
+	<div id="menu">
+		<div id="logo">
+			<a href="<?php echo site_url(); ?>">
+				<img src="<?php bloginfo('stylesheet_directory'); ?>/images/wfd_logo.png" />
+			</a>
+		</div>
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ); ?>
+		<div id="social">
+			<ul>
+				<li><img src="<?php bloginfo('stylesheet_directory'); ?>/images/facebook.png" /></li>
+			</ul>
+		</div>
+	</div>
+</div>
